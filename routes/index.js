f6108fcc30c1cd75af7fe4a4a51f14715ec2bfd0
Shahar Yogev <37019685+shaharyogev@users.noreply.github.com>
2018-12-02 21:04:31 +0200

    Code readability improved

--- routes/index.js
+++ routes/index.js
@@ -4,86 +4,81 @@ var fs = require('fs');
 var path = require('path');
 var async = require('async');
 
-
-function filesInSubFolder(obj, callback){
-
-  async.eachSeries(obj, function (nextFileInSub, callback){
-    dirNext.push({Name:nextFileInSub, Type:path.extname(nextFileInSub)});
+function filesInSubFolder(obj, callback) {
+  async.eachSeries(obj, function (nextFileInSub, callback) {
+    dirNext.push({
+      Name: nextFileInSub,
+      Type: path.extname(nextFileInSub)
+    });
     callback();
-
-  },function(err){
-    if(err){
-    console.log("Dir-Next file is not exsist! " + err);
-    }else{
-    //console.log("All the files in the SUB folder have been processed successfully");
-    }
+  }, function (err) {
+    if (err)
+      console.log("Dir-Next file is not exsist! " + err);
   });
+  callback()
+}
 
-  callback();
-};
-
-function filesInFolder(obj, callback){
+function filesInFolder(obj, callback) {
   var pcListFiles = [];
 
-  fs.readdir(obj,function(err,files){
-    if(err)
+  fs.readdir(obj, function (err, files) {
+    if (err)
       console.log("files is not exsist! " + err);
-    
-    async.eachSeries(files, function (file, callback){
+
+    async.eachSeries(files, function (file, callback) {
       dirNext = [];
-      fs.readdir(path.join(obj, file), function(err, nextFile){
-        if(err)
+      fs.readdir(path.join(obj, file), function (err, nextFile) {
+        if (err)
           console.log("nextFile is not exsist! " + err);
-        
+
         filesInSubFolder(nextFile, callback);
       });
 
-      pcListFiles.push({Name: file, Type: path.extname(file), Dir: dirNext});
+      pcListFiles.push({
+        Name: file,
+        Type: path.extname(file),
+        Dir: dirNext
+      });
 
-    },function(err){
-        if(err)
-          console.log("async file is not exsist! " + err);
-        
-        //console.log("All the files in MAIN folder have been processed successfully")
-        callback(pcListFiles);
-      }
-    );
-  });
-};
+    }, function (err) {
+      if (err)
+        console.log("async file is not exsist! " + err);
+
+      callback(pcListFiles);
+    })
+  })
+}
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
+  });
 });
 
-router.get('/pclistdata', function (req,res,next){
-  try{  
-    
-    filesInFolder("/",function(respond){
+router.get('/pclistdata', function (req, res, next) {
+  try {
+    filesInFolder("/", function (respond) {
       res.json(respond)
-      //console.log("The respond was send " + respond)
     });
-
-  }catch (error){
+  } catch (error) {
     console.log('Get did not work: ', error);
   }
 });
 
-router.post('/pclistdata', function(req,res){
-try{
+router.post('/pclistdata', function (req, res) {
+  try {
+    fileNameData = req.body.fileNameData;
+    countPath = req.body.countPath;
+    var filePath = JSON.parse(req.body.filePathJson);
 
-  fileNameData = req.body.fileNameData;
-  countPath = req.body.countPath;
-  var filePath = JSON.parse(req.body.filePathJson);
-  
-  filesInFolder(filePath[countPath],function(respond){
-    res.json(respond)
-    //console.log("The respond was send " + respond)
-  });
+    filesInFolder(filePath[countPath], function (respond) {
+      res.json(respond)
+    });
 
-}catch (error){
-  console.log('Post did not work: ' + filePath[countPath], error);
-}
+  } catch (error) {
+    console.log('Post did not work: ' + filePath[countPath], error);
+  }
 });
 
 
